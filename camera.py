2fae906303412e7d1676ac3845760ccac0e65417
Szymon Ochnio <szymonochnio15@gmail.com>
2024-04-09 19:53:04 +0200

    Minor improvements

--- camera.py
+++ camera.py
@@ -8,7 +8,7 @@ from calc import translate, rotate, project
 vww = 1280  # viewport width
 vwh = 720  # viewport height
 d = 400  # initial viewport
-t = 3  # translation step
+t = 2  # translation step
 r = np.pi / 64  # rotation step
 bg = 'black'
 fg = 'white'
@@ -24,7 +24,9 @@ cuboids = np.array([
     [[20, -50, 190], [70, -50, 190], [70, -50, 240], [20, -50, 240],
      [20, 50, 190], [70, 50, 190], [70, 50, 240], [20, 50, 240]]
 ], dtype=float)
-reset = copy.deepcopy(cuboids)
+
+cuboids_reset = copy.deepcopy(cuboids)
+d_reset = d
 
 pygame.init()
 pygame.display.set_caption('VCAM')
@@ -46,37 +48,41 @@ while running:
     if keys[pygame.K_ESCAPE]:
         running = False
     if keys[pygame.K_r]:
-        cuboids = copy.deepcopy(reset)
-    if keys[pygame.K_i]:
+        cuboids = copy.deepcopy(cuboids_reset)
+        d = d_reset
+    # zoom
+    if keys[pygame.K_i] and d <= 5000:
         d += 0.1 * d
-    if keys[pygame.K_o]:
+        print(d)
+    if keys[pygame.K_o] and d >= 1:
         d -= 0.1 * d
+        print(d)
+    # translations
     if keys[pygame.K_w]:
         tv[2] -= t
-    if keys[pygame.K_d]:
-        tv[0] -= t
-    if keys[pygame.K_s]:
-        tv[2] += t
     if keys[pygame.K_a]:
         tv[0] += t
+    if keys[pygame.K_s]:
+        tv[2] += t
+    if keys[pygame.K_d]:
+        tv[0] -= t
+    # rotations
     if keys[pygame.K_SPACE]:
         tv[1] += t
     if mods & pygame.KMOD_SHIFT:
         tv[1] -= t
     if keys[pygame.K_LEFT]:
-        if mods & pygame.KMOD_CTRL:
-            rv[2] += r
-        else:
-            rv[1] += r
+        rv[1] += r
     if keys[pygame.K_RIGHT]:
-        if mods & pygame.KMOD_CTRL:
-            rv[2] -= r
-        else:
-            rv[1] -= r
+        rv[1] -= r
     if keys[pygame.K_UP]:
         rv[0] -= r
     if keys[pygame.K_DOWN]:
         rv[0] += r
+    if keys[pygame.K_q]:
+        rv[2] += r
+    if keys[pygame.K_e]:
+        rv[2] -= r
 
     cuboids_2d = []
     for i in range(len(cuboids)):
