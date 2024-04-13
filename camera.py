f1affe50b60736c02a18925238f9134bf2c3288b
Szymon Ochnio <szymonochnio15@gmail.com>
2024-04-13 13:00:27 +0200

    Add naive centroid based covered elements elimination

--- camera.py
+++ camera.py
@@ -10,10 +10,11 @@ vwh = 720  # viewport height
 d = 400  # initial viewport
 t = 2  # translation step
 r = np.pi / 64  # rotation step
-bg = 'black'
-fg = 'white'
+bg = 'white'
+fg = 'black'
 
 edges = np.array([[0, 1], [1, 2], [2, 3], [3, 0], [4, 5], [5, 6], [6, 7], [7, 4], [0, 4], [1, 5], [2, 6], [3, 7]])
+faces = np.array([[0, 1, 2, 3], [4, 5, 6, 7], [0, 1, 5, 4], [1, 2, 6, 5], [2, 3, 7, 6], [3, 0, 4, 7]])
 cuboids = np.array([
     [[-70, -50, 100], [-20, -50, 100], [-20, -50, 150], [-70, -50, 150],
      [-70, 50, 100], [-20, 50, 100], [-20, 50, 150], [-70, 50, 150]],
@@ -82,17 +83,25 @@ while running:
     if keys[pygame.K_e]:
         rv[2] -= r
 
-    cuboids_2d = []
+    faces_2d = []
     for i in range(len(cuboids)):
         vertices_2d = []
         for j in range(len(cuboids[i])):
             cuboids[i][j] = rotate(translate(cuboids[i][j], tv), rv)
             vertices_2d.append(project(cuboids[i][j], d, vww, vwh))
-        cuboids_2d.append(vertices_2d)
+        for face in faces:
+            face_2d = []
+            centroid_z = 0.0
+            for vertex in face:
+                face_2d.append(vertices_2d[vertex])
+                centroid_z = centroid_z + cuboids[i][vertex][2]
+            centroid_z = centroid_z / 4.0
+            faces_2d.append([face_2d, centroid_z])
 
-    for i in range(len(cuboids_2d)):
-        for edge in edges:
-            pygame.draw.aaline(screen, fg, cuboids_2d[i][edge[0]], cuboids_2d[i][edge[1]])
+    faces_2d.sort(key=lambda x: x[1], reverse=True)
+    for face_2d in faces_2d:
+        pygame.draw.polygon(screen, bg, face_2d[0])
+        pygame.draw.lines(screen, fg, True, face_2d[0], 2)
 
     pygame.display.flip()
     dt = clock.tick(60) / 1000
